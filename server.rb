95c855eb73961a197f04e50c2f38576a1d664e89
Rob Fuller <679319+mubix@users.noreply.github.com>
2020-08-25 11:26:31 -0400

    add OPTIONS response

--- server.rb
+++ server.rb
@@ -4,6 +4,12 @@ require 'sinatra/webdav'
 class Server < Sinatra::Base
   register Sinatra::WebDAV
 
+  options '/*' do
+    response.headers['Allow'] = 'OPTIONS, LOCK, DELETE, PROPPATCH, COPY, MOVE, UNLOCK, PROPFIND'
+    response.headers['Dav'] = '1, 2'
+    response.headers['Ms-Author-Via'] = 'DAV'
+  end
+ 
   propfind '/netlogon/' do
     'wat'
   end
